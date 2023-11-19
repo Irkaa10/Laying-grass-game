c5081d838d6df286c3b6c280e35efd3571ecc2e1
Irkaa10 <149479479+Irkaa10@users.noreply.github.com>
2023-11-19 23:11:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,9 @@
 #include "helpers/displayColorChoices.hpp"
 #include "classes/board.hpp"
 
-int main()
+int getNumberOfPlayers(int &ARRAY_SIZE)
 {
     int nbPlayers;
-    int ARRAY_SIZE;
-
-    //Boucle pour demander le nombre de joueurs
-    // AJOUTER VÉRIF SI INPUT = INT
     while (true)
     {
         std::cout << "Players (2 to 9): ";
@@ -35,12 +31,51 @@ int main()
         break;
     }
 
-    // Demander aux joueurs leur nom et couleur
-    // Utiliser des vecteurs pour stocker les noms et couleurs des joueurs
-    std::vector<std::string> playerNames;
-    std::vector<std::string> playerColors;
+    return nbPlayers;
+}
 
-    // Utiliser un ensemble (set) pour suivre les couleurs déjà choisies
+std::string getColorName(int colorChoice)
+{
+    std::string color;
+    switch (colorChoice)
+    {
+    case 1:
+        color = "Red";
+        break;
+    case 2:
+        color = "Green";
+        break;
+    case 3:
+        color = "Blue";
+        break;
+    case 4:
+        color = "Yellow";
+        break;
+    case 5:
+        color = "Magenta";
+        break;
+    case 6:
+        color = "Cyan";
+        break;
+    case 7:
+        color = "White";
+        break;
+    case 8:
+        color = "Black";
+        break;
+    case 9:
+        color = "Orange";
+        break;
+    default:
+        std::cout << "Invalid color choice. Defaulting to Black." << std::endl;
+        color = "Black";
+        break;
+    }
+    return color;
+}
+
+void getPlayerInfo(int nbPlayers, std::vector<std::string> &playerNames, std::vector<std::string> &playerColors)
+{
     std::set<int> chosenColors;
 
     for (int i = 0; i < nbPlayers; ++i)
@@ -57,69 +92,44 @@ int main()
             std::cout << "Select color for " << playerName << " (1 to 9): ";
             std::cin >> colorChoice;
 
-            // Vérifier si la couleur est déjà choisie
             if (chosenColors.find(colorChoice) != chosenColors.end())
             {
                 std::cout << "Color already chosen. Please choose a different color." << std::endl;
             }
             else
             {
-                // Ajouter la couleur choisie à l'ensemble
                 chosenColors.insert(colorChoice);
-
-                // Associer le choix de couleur à une couleur réelle
-                std::string color;
-                switch (colorChoice)
-                {
-                case 1:
-                    color = "Red";
-                    break;
-                case 2:
-                    color = "Green";
-                    break;
-                case 3:
-                    color = "Blue";
-                    break;
-                case 4:
-                    color = "Yellow";
-                    break;
-                case 5:
-                    color = "Magenta";
-                    break;
-                case 6:
-                    color = "Cyan";
-                    break;
-                case 7:
-                    color = "White";
-                    break;
-                case 8:
-                    color = "Black";
-                    break;
-                case 9:
-                    color = "Orange";
-                    break;
-                default:
-                    std::cout << "Invalid color choice. Defaulting to Black." << std::endl;
-                    color = "Black";
-                    break;
-                }
-
-                playerColors.push_back(color);
+                playerColors.push_back(getColorName(colorChoice));
                 break;
             }
         }
     }
+}
 
-    // Afficher les noms et couleurs des joueurs
+void displayPlayerInfo(int nbPlayers, const std::vector<std::string> &playerNames, const std::vector<std::string> &playerColors)
+{
     for (int i = 0; i < nbPlayers; ++i)
     {
         std::cout << "Player " << i + 1 << ": " << playerNames[i] << " (Color: " << playerColors[i] << ")" << std::endl;
     }
+}
 
-    // Initialiser et afficher le tableau
+int main()
+{
+    int nbPlayers;
+    int ARRAY_SIZE;
+
+    nbPlayers = getNumberOfPlayers(ARRAY_SIZE);
+
+    std::vector<std::string> playerNames;
+    std::vector<std::string> playerColors;
+
+    getPlayerInfo(nbPlayers, playerNames, playerColors);
+
+    displayPlayerInfo(nbPlayers, playerNames, playerColors);
+
     Board newBoard(ARRAY_SIZE);
     newBoard.displayBoard();
-
 
     return 0;
 }
