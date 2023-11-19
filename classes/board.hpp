13a3e125657be2fccf0ba048c492f32f53aca670
Irkaa10 <149479479+Irkaa10@users.noreply.github.com>
2023-11-19 18:12:13 +0100

    Update board.hpp

--- classes/board.hpp
+++ classes/board.hpp
@@ -16,32 +16,14 @@ public:
 
 Board::Board(int size)
 {
-    this->size = size + 1; // Augmentez la taille pour ajouter une ligne et une colonne pour les nombres
-    array = new char *[this->size];
-    for (int i = 0; i < this->size; ++i)
-    {
-        array[i] = new char[this->size];
-        for (int j = 0; j < this->size; ++j)
-        {
-            if (i == 0) // Première ligne pour les nombres de colonnes
-            {
-                array[i][j] = j == 0 ? ' ' : char('0' + j);
-            }
-            else if (j == 0) // Première colonne pour les nombres de lignes
-            {
-                array[i][j] = char('0' + i);
-            }
-            else
-            {
-                array[i][j] = '.';
-            }
+    // Initialiser la taille
+    this->size = size;
 
-            if (array[i][j] > '9')
-            {
-                // Si la valeur est supérieure à '9', remplacez-la par un entier
-                array[i][j] = char('0' + i);
-            }
-        }
+    // Allouer de la mémoire pour le tableau
+    array = new char *[size];
+    for (int i = 0; i < size; ++i)
+    {
+        array[i] = new char[size];
     }
 }
 
