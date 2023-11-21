6d525c69f27ea28ff7aa374e23878da4ef0ae502
Irkaa10 <149479479+Irkaa10@users.noreply.github.com>
2023-11-21 16:53:06 +0100

    Update board.hpp

--- classes/board.hpp
+++ classes/board.hpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <vector>
 
 class Board
 {
 private:
     int boardSize;
-    std::vector<std::vector<char> > board;
+    char **board;
 
 public:
     Board(int size);
@@ -15,39 +14,68 @@ public:
     void fillCell(int row, int col);
 };
 
-// constructor
-Board::Board(int size) : boardSize(size), board(size, std::vector<char>(size, '.')) {}
-
-// Default destructor
-Board::~Board() = default;
-
-void Board::displayBoard() const
+// constructor to initialize the board size and allocate memory for the 2D array
+Board::Board(int size) : boardSize(size)
 {
+    // Allocate memory for the 2D array
+    board = new char *[size];
+    for (int i = 0; i < size; ++i)
     {
-        // Print column coordinates with letters
-        std::cout << "   ";
-        for (int i = 0; i < boardSize; ++i)
-        {
-            std::cout << char('A' + i) << " ";
-        }
-        std::cout << std::endl;
+        board[i] = new char[size];
+    }
 
-        // Print rows with row coordinates, letters, and array elements
-        for (int i = 0; i < boardSize; ++i)
+    // Initialize the board
+    initBoard();
+}
+
+// Destructor to free the allocated memory
+Board::~Board()
+{
+    for (int i = 0; i < boardSize; ++i)
+    {
+        delete[] board[i];
+    }
+    delete[] board;
+}
+
+// Function to initialize the board with '.' in each cell
+void Board::initBoard()
+{
+    for (int i = 0; i < boardSize; ++i)
+    {
+        for (int j = 0; j < boardSize; ++j)
         {
-            std::cout << char('A' + i) << "  ";
-            for (int j = 0; j < boardSize; ++j)
-            {
-                std::cout << board[i][j] << " ";
-            }
-            std::cout << std::endl;
+            board[i][j] = '.';
         }
     }
 }
 
+void Board::displayBoard() const
+{
+    // Print column coordinates with letters
+    std::cout << "   ";
+    for (int i = 0; i < boardSize; ++i)
+    {
+        std::cout << char('A' + i) << " ";
+    }
+    std::cout << std::endl;
+
+    // Print rows with row coordinates, letters, and array elements
+    for (int i = 0; i < boardSize; ++i)
+    {
+        std::cout << char('A' + i) << "  ";
+        for (int j = 0; j < boardSize; ++j)
+        {
+            std::cout << board[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// Function to fill a cell with 'X' at the specified coordinates (int row, int col).
 void Board::fillCell(int row, int col)
 {
-    if (row >= 0 && row < boardSize && col >= 0 && col < boardSize)
+    if (row >= 0 && row < boardSize && col >= 0 && col < boardSize) // Check if coordinates are valid (within the board)
     {
         board[row][col] = 'X';
     }
