f48f940b7bf2972062b635cc99b5527512074e56
Irkaa10 <149479479+Irkaa10@users.noreply.github.com>
2023-11-21 16:46:11 +0100

    Update tiles.hpp

--- classes/tiles.hpp
+++ classes/tiles.hpp
@@ -1,74 +1,125 @@
 #include <iostream>
-#include <fstream> //on utilise fstream afin de lire dans un fichier texte directement les dimensions des différentes tuiles
+#include <fstream>
+#include <sstream>
+#include <vector>
 
-class Tile
+struct Coordinate
+{
+    int x, y; // x and y coordinates
+};
+
+struct Tile
+{
+    char id;                             // Tile ID
+    std::vector<Coordinate> coordinates; // Coordinates of the tile
+};
+
+class TilesManager
 {
 private:
-    //la taille sur laquelle s'étant la tile (par exemple, si la tile fait 3x4 x=3 donc si posé en x=10 sur plateau alors la tile recouvrera x=10, 11 et 12).
-    //de même pour y qui représente donc l'axe Y du plateau.
-    int x;
-    int y;
+    std::vector<Tile> tiles; // Vector to store the tiles
+
 public:
-    Tile(int x, int y) : x(x); y(y) {}
-
-    //On définir getX et getY, afin d'accéder aux valeurs de la Tile dans la classe qui permettra de les définir
-    //Par mesure de sécurité, ces fonctions sont crées pour que les joueurs ne puissent définir les dimensions de leur Tile
-    //Elles sont donc uniquement accessible via des fonctions et non directement en public via leur valeur
-
-    int getX() const {
-        return x;
-    }
-
-    int getY() const {
-        return y;
-    }
+    // TilesManager(const std::string &filename);   // Constructor to read the tiles from a file
+    std::vector<Tile> readTilesFromFile(const std::string &filename); // Function to return the vector of tiles
+    void displayTile(const Tile &tile);                               // Function to display the tiles
+    void handleRotation(Tile &tile);                                  // Function to rotate the tiles
+    void displayQueue();                                              // Function to display the next 5 tiles
 };
 
-//permet de définir les dimensions d'une tuile par rapport a sa taille
-
-class CustomTile :: public Tile
+// Function to read tiles from a file and store them in a vector of Tile objects
+std::vector<Tile> TilesManager::readTilesFromFile(const std::string &filename)
 {
-public:
-    CustomTile(int x, int y) : Tile(x, y) {}
-};
+    std::vector<Tile> tiles;      // Vector to store the tiles read from the file
+    std::ifstream file(filename); // Open the file
 
-//fonction qui permet de créer par rapport aux nombres de joueurs le nombre parfait de custom tile qu'il faut
-// l'argument N°2 : le nombre de joueur sert a définir le nombre maximum de tuiles présent dans le vecteur afin de ne pas surchagé le nombre d'objets a l'intérieur
-// l'argument N°3 : argument fixe qui permettrait de définir que chaque joueur a maximum 10.67 tuiles a ramené par sa présence
-
-std::vector<CustomTile> ReadCustomTilesDimensions(const std::string& filename, int NbPlayers, int const TilesMAX)
-{
-    std::vector<CustomTile> CustomTiles;
-    std::ifstream file(filename);
-
-    //bien définir des tuiles 1x1 présent au début du vecteur par rapport aux nombres de joueurs, afin que chaque premier round de chaque joueur soit bien des tiles 1x1
-
-    int defaultNbPlayers = NbPlayers;
-
-    while (defaultNbPlayers > 0)
+    if (file.is_open()) // Check if the file is open
     {
-        CustomTile customtile(1,1);
-        Customtiles.push_back(customtile);
-        --defaultNbPlayers;
-    }
-
-    if (file.is_open())
-    {
-        int x, y;
-        int TilesToRead = TilesMAX * NbPlayers;
-        //Par rapport aux nombres de joueurs, le vecteur contiendra le nombre de tuiles nécessaire
-        while (TilesToRead > 0 && file >> x >> y)
+        std::string line;                // String to store each line read from the file
+        while (std::getline(file, line)) // Read each line from the file
         {
-            CustomTile customTile(x,y);
-            CustomTiles.push_back(customTile);
-            --TilesToRead;
+            if (line.substr(0, 5) == "Tile:") // Check if the line starts with "Tile:" by comparing the first 5 characters of the line
+            {
+                Tile tile;         // Create a Tile object to store the tile
+                tile.id = line[6]; // Get the tile ID from the line (the 7th character)
+
+                while (std::getline(file, line) && !line.empty()) // Read the coordinates of the tile until an empty line is encountered or the end of the file is reached
+                {
+                    std::istringstream iss(line);      // Create an input string stream to read the coordinates from the line
+                    Coordinate coord;                  // Create a Coordinate object to store the coordinates
+                    iss >> coord.x >> coord.y;         // Read the coordinates from the string stream and store them in the Coordinate object
+                    tile.coordinates.push_back(coord); // Add the coordinates to the vector of coordinates
+                }
+
+                tiles.push_back(tile); // Add the tile to the vector of tiles
+            }
         }
+
         file.close();
     }
     else
     {
-        std::cout << "Impossible d'ouvrir le fichier: " << filename << std::endl;
+        std::cerr << "Unable to open file: " << filename << std::endl;
     }
 
-    return CustomTiles;
+    return tiles; // Return the vector of tiles
+}
+
+// Function to rotate a tile 90 degrees clockwise
+void TilesManager::handleRotation(Tile &tile) // Pass by reference to modify the original tile
+{
+    for (auto &coord : tile.coordinates) // Loop through each coordinate of the tile
+    {
+        // Rotate coordinates (x, y) to (y, -x)
+        int temp = coord.x; // Store x in a temporary variable
+        coord.x = coord.y;  // Set x to y
+        coord.y = -temp;    // Set y to -x
+    }
+}
+
+// Function to display the next 5 tiles in the queue (The last 5 tiles in the vector)
+void TilesManager::displayQueue()
+{
+    int startIdx = std::max(0, static_cast<int>(tiles.size()) - 5); // Start index for the last 5 tiles
+
+    std::cout << "Next tiles:" << std::endl;
+    for (int i = startIdx; i < tiles.size(); ++i) // Loop through the last 5 tiles in the vector of tiles
+    {
+        displayTile(tiles[i]); // Display the tile
+    }
+}
+
+// Function to display the graphical representation of a tile on a grid
+void TilesManager::displayTile(const Tile &tile)
+{
+    // Find the dimensions of the tile to determine the grid size
+    int minX = 0, minY = 0, maxX = 0, maxY = 0; // Initialize the min and max coordinates to the first coordinate of the tile
+    for (const auto &coord : tile.coordinates)  // Loop through each coordinate of the tile
+    {
+        minX = std::min(minX, coord.x); // Update the min and max coordinates
+        minY = std::min(minY, coord.y);
+        maxX = std::max(maxX, coord.x);
+        maxY = std::max(maxY, coord.y);
+    }
+
+    // Create an empty grid to display the tile
+    std::vector<std::vector<char>> grid(maxY - minY + 1, std::vector<char>(maxX - minX + 1, ' '));
+
+    // Mark the tile coordinates on the grid
+    for (const auto &coord : tile.coordinates)
+    {
+        grid[coord.y - minY][coord.x - minX] = '#';
+    }
+
+    // Display the grid
+    for (int y = 0; y <= maxY - minY; ++y)
+    {
+        for (int x = 0; x <= maxX - minX; ++x)
+        {
+            std::cout << grid[y][x] << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
 }
