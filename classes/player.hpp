71159a56e7d620972bce6df7b9e4b489d02e17d9
Irkaa10 <149479479+Irkaa10@users.noreply.github.com>
2023-11-23 00:54:06 +0100

    Update player.hpp

--- classes/player.hpp
+++ classes/player.hpp
@@ -6,12 +6,14 @@ class Player
 private:
     std::string name;
     int color;
+    std::vector<std::pair<int, int> > territory;
 
 public:
     // Constructeur prenant le nom et la couleur en paramètres
     Player(const std::string &playerName, const int playerColor);
     // Méthode pour afficher les informations du joueur
     void displayInfo() const;
+    void addTerritory(int x, int y, int boardsSize);
 };
 
 // Implémentation du constructeur
@@ -20,4 +22,31 @@ Player::Player(const std::string &playerName, const int playerColor) : name(play
 void Player::displayInfo() const
 {
     std::cout << "Player " << name << " (Color: " << color << ")" << std::endl;
+}
+
+void Player::addTerritory(int x, int y, int boardSize)
+{
+    // Add the given cell if it is within bounds
+    if (x >= 0 && x < boardSize && y >= 0 && y < boardSize)
+    {
+        territory.emplace_back(x, y);
+
+        // Add neighboring cells if they are within bounds
+        if (x + 1 >= 0 && x + 1 < boardSize && y >= 0 && y < boardSize)
+            territory.emplace_back(x + 1, y);
+        if (x - 1 >= 0 && x - 1 < boardSize && y >= 0 && y < boardSize)
+            territory.emplace_back(x - 1, y);
+        if (x >= 0 && x < boardSize && y + 1 >= 0 && y + 1 < boardSize)
+            territory.emplace_back(x, y + 1);
+        if (x >= 0 && x < boardSize && y - 1 >= 0 && y - 1 < boardSize)
+            territory.emplace_back(x, y - 1);
+        if (x + 1 >= 0 && x + 1 < boardSize && y + 1 >= 0 && y + 1 < boardSize)
+            territory.emplace_back(x + 1, y + 1);
+        if (x - 1 >= 0 && x - 1 < boardSize && y - 1 >= 0 && y - 1 < boardSize)
+            territory.emplace_back(x - 1, y - 1);
+        if (x + 1 >= 0 && x + 1 < boardSize && y - 1 >= 0 && y - 1 < boardSize)
+            territory.emplace_back(x + 1, y - 1);
+        if (x - 1 >= 0 && x - 1 < boardSize && y + 1 >= 0 && y + 1 < boardSize)
+            territory.emplace_back(x - 1, y + 1);
+    }
 }
